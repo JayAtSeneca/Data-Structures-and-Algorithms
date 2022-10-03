a0debfde3f08871367a590f2a9ba734746534769
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-03 17:23:00 -0400

    factorial using recursion done

---
+++ lab03/factorial.py
@@ -0,0 +1,8 @@
+def factorial(number):
+    if number <= 1:
+        return 1
+    return number * (factorial(number-1))
+print(factorial(0),1)
+print(factorial(1),1)
+print(factorial(19),121645100408832000)
+print(factorial(8),40320)
