d0d39e13742d48daa6124553cd05823b6977b053
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-16 14:04:23 -0400

    understanding yield keyword in python

--- assignment Practice/classInPy.py
+++ assignment Practice/classInPy.py
@@ -1,12 +1,19 @@
-class Length:
-    def __init__(self, len=None):
-        if len != None:
-            self.length = len
-        else:
-            self.length = 0
+# class Length:
+#     def __init__(self, len=None):
+#         if len != None:
+#             self.length = len
+#         else:
+#             self.length = 0
     
-    def __len__(self):
-        return self.length
+#     def __len__(self):
+#         return self.length
 
-my_len = Length(10)
-print(len(my_len))
+# my_len = Length(10)
+# print(len(my_len))
+def custom_generator():
+    mylist = range(3)
+    for i in mylist:
+        yield i*i
+my_gen = custom_generator()
+for i in my_gen:
+    print(i)
