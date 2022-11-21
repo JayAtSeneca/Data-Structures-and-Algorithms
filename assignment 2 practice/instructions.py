1168091f8a6fb8ccce0ef9509a6763d8d6c646fd
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-11-21 02:43:02 -0500

    checking the between condition

--- assignment 2 practice/instructions.py
+++ assignment 2 practice/instructions.py
@@ -1,8 +1,5 @@
-# cap = 32
-# idx = hash("apple")%32
-# new_idx = hash("apple")%64
-# print(idx)
-# print(new_idx)
-my_list = [1,1,2,3,4]
-my_list.remove(5)
-print(my_list)
+number = 9
+if 10<=number<=60:
+    print("present sir")
+else:
+    print("Not present")
